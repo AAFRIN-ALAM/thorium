88260868bd9474431b07251c5c82c856f8dec603
Aafrin <aafrinalam157@gmail.com>
2022-03-01 03:51:57 +0530

    mongodb2_part2

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,14 +1,14 @@
-const { count } = require("console")
-const BookModel= require("../models/bookModel")
+// const { count } = require("console")
+// const BookModel= require("../models/bookModel")
 
-const createBook= async function (req, res) {
-    let data= req.body
+// const createBook= async function (req, res) {
+//     let data= req.body
 
-    let savedData= await BookModel.create(data)
-    res.send({msg: savedData})
-}
+//     let savedData= await BookModel.create(data)
+//     res.send({msg: savedData})
+// }
 
-const getBooksData= async function (req, res) {
+// const getBooksData= async function (req, res) {
 
     // let allBooks= await BookModel.find( ).count() // COUNT
 
@@ -65,21 +65,124 @@ const getBooksData= async function (req, res) {
     
     // ASYNC AWAIT
     
-    let a= 2+4
-    a= a + 10
-    console.log(a)
-    let allBooks= await BookModel.find( )  //normally this is an asynchronous call..but await makes it synchronous
+    // let a= 2+4
+    // a= a + 10
+    // console.log(a)
+    // let allBooks= await BookModel.find( )  //normally this is an asynchronous call..but await makes it synchronous
 
 
     // WHEN AWAIT IS USED: - database + axios
     //  AWAIT can not be used inside forEach , map and many of the array functions..BE CAREFUL
-    console.log(allBooks)
-    let b = 14
-    b= b+ 10
-    console.log(b)
-    res.send({msg: allBooks})
+//     console.log(allBooks)
+//     let b = 14
+//     b= b+ 10
+//     console.log(b)
+//     res.send({msg: allBooks})
+// }
+
+
+// module.exports.createBook= createBook
+// module.exports.getBooksData= getBooksData
+
+
+
+const { count } = require("console")
+const BookModel= require("../models/bookModel")
+
+const createBook = async function(request, response){
+    const data = request.body; 
+    const dataRes = await BookModel.create(data); 
+    response.send({
+        msg: dataRes
+    }); 
+}
+
+const bookList = async function(request, response){
+    const data = request.body;
+    const dataRes = await BookModel.find(data).select({
+        'bookName': 1,
+        'authorName': 1,
+        '_id': 0
+    }); 
+    response.send({
+        msg: dataRes
+    }); 
+}
+
+const getBooksInYear = async function(request, response){
+    const year = request.body.year; 
+    const dataRes = await BookModel.find({
+        'year': year
+    }).select({
+        'bookName': 1,
+        '_id': 0
+    }); 
+    response.send({
+        msg: dataRes
+    });
+}
+
+const getParticularBooks = async function(request, response){
+    const data = request.body; 
+    const dataRes = await BookModel.find(data).select({
+        'bookName': 1,
+        'year':1,
+        'authorName':1,
+        '_id': 0,
+    }); 
+    response.send({
+        msg: dataRes
+    });
 }
 
 
-module.exports.createBook= createBook
-module.exports.getBooksData= getBooksData
+const getRandomBooks = async function(request, response){
+    const data = await BookModel.find({
+        $and: [
+            {
+                'totalPages':{
+                    $gt: 500
+                }
+            },
+            {
+                'stockAvailable': true
+            }
+        ]
+    }); 
+    response.send({
+        msg: data
+    });
+}
+
+
+const getXINRBooks = async function(request, response){
+    const data = request.body;
+    const dataRes = await BookModel.find({
+        $or: [
+            {
+                "prices.indianPrice": "100 INR"
+            },
+            {
+                "prices.indianPrice": "200 INR"
+            },
+            {
+                "prices.indianPrice": "500 INR"
+            },
+        ]
+    }); 
+    response.send({
+        msg: dataRes
+    });
+}
+
+
+
+
+module.exports = {
+    createBook: createBook,
+    bookList: bookList,
+    getBooksInYear: getBooksInYear,
+    getParticularBooks: getParticularBooks,
+    getXINRBooks: getXINRBooks,
+    getRandomBooks: getRandomBooks
+}
--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,20 +1,56 @@
+// const mongoose = require('mongoose');
+
+// const bookModel = new mongoose.Schema( {
+//     bookName: {
+//         type : String, 
+//         required : true
+//     },
+//     year: {
+//         type : Number,
+//         required : true
+//     },
+//     authorName: {
+//         type: String,
+//         required: true
+//     }, 
+//     tags: [String],
+    
+//     isPublished: Boolean,
+//     prices: {
+//         indianPrice: String,
+//         europePrice: String,
+//     },
+//     sales: {type: Number, default: 10}
+// }, { timestamps: true });
+
+
+
 const mongoose = require('mongoose');
 
 const bookSchema = new mongoose.Schema( {
-    bookName: String, 
-    authorName: String, 
-    tags: [String],
-    
-    isPublished: Boolean,
+    bookName: {
+        type: String,
+        required: true
+    }, 
     prices: {
         indianPrice: String,
         europePrice: String,
+    }, 
+    year: {
+        type: Number,
+        default: 2021
     },
-    sales: {type: Number, default: 10}
+    tags: [String],
+    // authorName: String, 
+    // totalPages: String,
+    // stockAvailable: String,
 }, { timestamps: true });
 
 
-module.exports = mongoose.model('Book', bookSchema) //users
+module.exports = mongoose.model('Book', bookSchema)
+
+
+//module.exports = mongoose.model('Book', bookModel) //users
 
 //Validation:
 //require:true
