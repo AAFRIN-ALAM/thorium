5aee9c1e9b1cc3995f3c699eb29d733c0898310a
Aafrin <aafrinalam157@gmail.com>
2022-03-02 10:11:04 +0530

    mongodb-session3

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,54 +1,16 @@
-const express = require("express");
+const express = require('express');
+
 const router = express.Router();
 
-let persons = [
-  {
-      name: "Anjlai",
-      age: 10,
-      votingStatus: false
-  },
-  {
-      name: "Priti",
-      age: 20,
-      votingStatus: false
-  },
-  {
-      name: "Ayush",
-      age: 70,
-      votingStatus: false
-  },
-  {
-      name: "Subham",
-      age: 25,
-      votingStatus: false
-  },
-  {
-      name: "Himashu",
-      age: 30,
-      votingStatus: false
-  }
-]
 
-router.post("/voting-post", function (req, res) {
-  let votingAge = req.query.votingAge
+const controller = require('../controllers/controller')
 
-  let newArray=[];
-  for (let i = 0; i < persons.length; i++) {
 
-      if (persons[i].age > votingAge) {
-      
-          persons[i].votingStatus = true
-          newArray.push(persons[i])
-      }
-  }
-if (newArray.length>0)
-{
-  return res.send(newArray)
-}
-else{
-  return res.send("no member found above this age")
-}
 
-})
+router.post('/createAuthor', controller.createAuthor)
+router.post('/createBook', controller.createBook)
+router.get('/bookByChetan', controller.bookByChetan)
+router.post('/priceUpdate', controller.priceUpdate)
+router.get('/booksInPrice', controller.booksInPrice)
 
 module.exports = router;
