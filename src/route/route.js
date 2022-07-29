d44587cece57cccf0bd8632f30eee823ed17a367
AAFRIN-ALAM <98942359+AAFRIN-ALAM@users.noreply.github.com>
2022-07-29 21:53:18 +0530

    Update route.js

--- src/route/route.js
+++ src/route/route.js
@@ -1,6 +1,6 @@
 const express=require('express')
 const router=express.Router()
-const dictionaryController=('../controller/dictonaryController')
+const dictionaryController=require('../controller/dictonaryController')
 
 router.get("/api/v2/entries/{source_lang}/{word_id}", dictionaryController.fetchData)
 
